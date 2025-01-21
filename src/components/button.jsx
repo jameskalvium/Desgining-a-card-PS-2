3a479bb614ba6b811819ef0466c034e0197c3965
unknown <james.r@kalvium.community>
2025-01-21 15:34:35 +0530

    made changes

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,1 +1,20 @@
 // write button card here
+
+import React from 'react';
+
+const ViewProduct = () => {
+  const buttonText = 'View Product';
+
+  const button = {
+    padding: '8px 16px',
+    backgroundColor: 'blue',
+    border: 'none',
+    color: 'white',
+    borderRadius: '10px'
+    
+  };
+
+  return <button style={button}>{buttonText}</button>;
+};
+
+export default ViewProduct;
--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,1 +1,29 @@
-// write product card here
+import React from 'react';
+import ViewProductButton from './button.jsx'; 
+
+const Productcard = () => {
+  const Image =
+    'https://m.media-amazon.com/images/I/71oWrfCTiqL._SL1500_.jpg';
+  const productName = 'Iphone 16';
+  const price = '899';
+
+
+  const image = {
+    width: '100%',
+    height: '200px',
+    objectFit: 'cover',
+    borderRadius: '10px'
+  };
+
+
+  return (
+    <div >
+      <img src={Image} alt="Product" style={image} />
+      <h2 >{productName}</h2>
+      <p style={{color:"white"}}>{price}</p>
+      <ViewProductButton />
+    </div>
+  );
+};
+
+export default Productcard;
